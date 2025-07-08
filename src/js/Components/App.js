cda6b36becfb624d14d9ddbb541095384e29f00a
rpritr <robi@foreach-labs.si>
2025-07-08 12:10:53 +0200

    add data object to header

--- src/js/Components/App.js
+++ src/js/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "../../styles/main.scss";
 import Widget from "./WeatherWidget/Widget";
+import data from "../data/weather";
+console.log(data);
 const App = () => {
     return (
         <>
             <h1>Test</h1>
-            <Widget>
+            <Widget data={data[0]}>
 
             </Widget>
         </>
--- src/js/Components/WeatherWidget/Widget.js
+++ src/js/Components/WeatherWidget/Widget.js
@@ -3,9 +3,11 @@ import styles from "./Widget.scss";
 import Header from "./Header";
 import HourlyForecast from "./HourlyForecast";
 const Widget = (props) => {
+    const {city, current_temperature, state} = props.data;
+    console.log(city);
     return (
         <section className="widget__section">
-            <Header></Header>
+            <Header city={city} temp={current_temperature} state={state}></Header>
             <HourlyForecast/>
         </section>
     )
