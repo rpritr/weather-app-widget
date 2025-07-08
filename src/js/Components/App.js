aa865e32052e7b3a7433532b58fe5afc4440caa4
rpritr <robi@foreach-labs.si>
2025-07-08 11:19:45 +0200

    initial setup

---
+++ src/js/Components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/main.scss";
+import Widget from "./WeatherWidget/Widget";
+const App = () => {
+    return (
+        <>
+            <h1>Test</h1>
+            <Widget>
+
+            </Widget>
+        </>
+    )
+}
+
+export default App;
